7af8f0180035ce6f987dc08cdc7ec4cc400714e2
yoel <yoelgiraldo34@gmail.com>
2026-03-18 11:29:03 -0500

    mi primer commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mi_nuevo_proyecto_commit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sumar dos numeros");
+            int a, b, x;
+            Console.WriteLine("ingrese el primer numero");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese el segundo numero");
+            b = int.Parse(Console.ReadLine());
+            x = a + b;
+            Console.Write($"la suma de los dos numeros es: {x}");
+            
+            
+        }
+    }
+}
